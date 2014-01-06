3ebebf1eca20b5bcf7253602e1e2de3e321626f6
adrienbd <adrienbd1@gmail.com>
2014-01-06 11:56:07 +0100

    Complete linear generator with MID and HARD difficulty, first version of random generator with EASY difficulty

--- src/generator/DataGenerator.java
+++ src/generator/DataGenerator.java
@@ -3,6 +3,8 @@
  */
 package generator;
 
+import java.util.Random;
+
 import model.Plane;
 
 /**
@@ -23,34 +25,69 @@ public class DataGenerator {
 	 * Number of minutes while planes can land.
 	 */
 	public static final int LANDING_WINDOW_END = 1050;
-	
+
 	public static final int MIN_PARKING_TIME = 30;
 	public static final int MAX_PARKING_TIME = 270;
-	
+
 	public static final int EASY = 10;
-	public static final int MEDIUM = 11;
+	public static final int MID = 11;
 	public static final int HARD = 12;
 
-	public Plane[] generateLinear(int numberOfFlights, int weightDifficulty) {
+	public Plane[] generateLinear(int numberOfFlights, int difficulty) {
 		Plane[] planes = new Plane[numberOfFlights];
 		int window = LANDING_WINDOW_END / numberOfFlights;
-		for (int i = 0; i < numberOfFlights; i ++) {
-			switch (weightDifficulty) {
+		Random random = new Random();
+		for (int i = 0; i < numberOfFlights; i++) {
+			switch (difficulty) {
 			case EASY:
-				planes[i] = new Plane(Plane.LIGHT,i*window,Math.min(i*window + MAX_PARKING_TIME, HIGHEST_TIME));
+				planes[i] = new Plane(Plane.LIGHT, i * window, Math.min(i
+						* window + MAX_PARKING_TIME, HIGHEST_TIME));
+				break;
+			case MID:
+				int weight = random.nextInt(2) + 1;
+				planes[i] = new Plane(weight, i * window, Math.min(i * window
+						+ MAX_PARKING_TIME, HIGHEST_TIME));
+				break;
+			case HARD:
+				weight = random.nextInt(3) + 1;
+				planes[i] = new Plane(weight, i * window, Math.min(i * window
+						+ MAX_PARKING_TIME, HIGHEST_TIME));
 				break;
 
 			default:
-				planes[i] = new Plane(1,i*window,Math.min(i*window + MAX_PARKING_TIME, HIGHEST_TIME));
+				planes[i] = new Plane(1, i * window, Math.min(i * window
+						+ MAX_PARKING_TIME, HIGHEST_TIME));
 				break;
 			}
 		}
 		return planes;
 	}
-	
+
+	public Plane[] generateRandom(int numberOfFlights, int difficulty) {
+		Plane[] planes = new Plane[numberOfFlights];
+		Random random = new Random();
+		for (int i = 0; i < numberOfFlights; i++) {
+			switch (difficulty) {
+			case EASY:
+				int landing = random.nextInt(LANDING_WINDOW_END + 1);
+				planes[i] = new Plane(Plane.LIGHT, landing, Math.min(landing
+						+ MAX_PARKING_TIME, HIGHEST_TIME));
+				break;
+
+			default:
+				landing = random.nextInt(LANDING_WINDOW_END + 1);
+				planes[i] = new Plane(Plane.LIGHT, landing, Math.min(landing
+						+ MAX_PARKING_TIME, HIGHEST_TIME));
+				break;
+			}
+		}
+
+		return planes;
+	}
+
 	public static void main(String[] args) {
 		DataGenerator generator = new DataGenerator();
-		Plane[] planes = generator.generateLinear(25, EASY);
+		Plane[] planes = generator.generateRandom(5, EASY);
 		for (Plane plane : planes) {
 			System.out.println(plane);
 		}
